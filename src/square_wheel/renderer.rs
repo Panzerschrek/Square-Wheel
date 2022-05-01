e996a635d26a1e13067c949632bf7da3bb09bcd8
Panzerschrek <AntiPanzerschrek@yandex.ru>
2022-05-01 11:17:26 +0300

    Pri podgotovke poverhnosti umnožajem çvet iz tekstury na znacenije osvescenija. Osvescenije poka cto eto prosto konstanta.

--- src/square_wheel/renderer.rs
+++ src/square_wheel/renderer.rs
@@ -728,8 +728,8 @@ impl Renderer
 					surface_size,
 					polygon_data.surface_tc_min,
 					&self.textures[polygon.texture as usize][polygon_data.mip as usize],
-					&mut self.surfaces_pixels
-						[surface_pixels_offset .. (surface_pixels_offset + ((surface_size[0] * surface_size[1]) as usize))],
+					&mut self.surfaces_pixels[surface_pixels_offset ..
+						(surface_pixels_offset + ((surface_size[0] * surface_size[1]) as usize))],
 				);
 			}
 		}
--- src/square_wheel/surfaces.rs
+++ src/square_wheel/surfaces.rs
@@ -7,6 +7,8 @@ pub fn build_surface(
 	out_surface_data: &mut [Color32],
 )
 {
+	let light = [1.5, 1.4, 1.3];
+
 	for dst_y in 0 .. surface_size[1]
 	{
 		let dst_line_start = (dst_y * surface_size[0]) as usize;
@@ -18,7 +20,20 @@ pub fn build_surface(
 		let mut src_x = surface_tc_min[0].rem_euclid(texture.size[0] as i32);
 		for dst_x in 0 .. surface_size[0]
 		{
-			dst_line[dst_x as usize] = src_line[src_x as usize];
+			let texel_value = src_line[src_x as usize];
+
+			let components = texel_value.unpack_to_rgb_f32();
+			let components_modulated = [
+				(components[0] * light[0]).min(Color32::MAX_RGB_F32_COMPONENTS[0]),
+				(components[1] * light[1]).min(Color32::MAX_RGB_F32_COMPONENTS[1]),
+				(components[2] * light[2]).min(Color32::MAX_RGB_F32_COMPONENTS[2]),
+			];
+
+			// Here we 100% sure that components overflow is not possible (because of "min").
+			// NaNs are not possible here too.
+			let color_packed = unsafe { Color32::from_rgb_f32_unchecked(&components_modulated) };
+
+			dst_line[dst_x as usize] = color_packed;
 			src_x += 1;
 			if src_x == (texture.size[0] as i32)
 			{
