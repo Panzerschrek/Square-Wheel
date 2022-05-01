750a65147e4c2272406d4dbf88723a93d28830a1
Panzerschrek <AntiPanzerschrek@yandex.ru>
2022-05-01 10:13:00 +0300

    Ispoljzujem boleje bystryj kod vycislenija usrednönnogo çveta iz cetyröh dlä generaçii mip-urovnej tekstur.

--- src/common/color.rs
+++ src/common/color.rs
@@ -21,19 +21,22 @@ impl Color32
 	pub fn get_average_4(colors: [Color32; 4]) -> Self
 	{
 		let mut components_sum = [0u32; 4];
+		// Calculate sum value of unshifted components, than shift and mask it on order to get average value.
+		// Exception - last component, which is shifted in oredr to prevent u32 overflow.
+		// Such approach uses less instructions relative to naive approach with separate shift of components to range 0-255.
 		for color in colors
 		{
-			components_sum[0] += (color.0 & 255) as u32;
-			components_sum[1] += ((color.0 >> 8) & 255) as u32;
-			components_sum[2] += ((color.0 >> 16) & 255) as u32;
-			components_sum[3] += (color.0 >> 24) as u32;
+			components_sum[0] += (color.0 & 0x000000FF) as u32;
+			components_sum[1] += (color.0 & 0x0000FF00) as u32;
+			components_sum[2] += (color.0 & 0x00FF0000) as u32;
+			components_sum[3] += ((color.0 & 0xFF000000) >> 2) as u32;
 		}
 
 		Color32(
-			(components_sum[3] >> 2 << 24) |
-				(components_sum[2] >> 2 << 16) |
-				(components_sum[1] >> 2 << 8) |
-				(components_sum[0] >> 2),
+			(components_sum[0] >> 2) |
+				((components_sum[1] >> 2) & 0x0000FF00) |
+				((components_sum[2] >> 2) & 0x00FF0000) |
+				(components_sum[3] & 0xFF000000),
 		)
 	}
 }
