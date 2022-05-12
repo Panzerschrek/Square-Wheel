166b60d0f8616af7033bbbe1fde65f7473f5b038
Panzerschrek <AntiPanzerschrek@yandex.ru>
2022-05-12 21:47:12 +0300

    Realizovan, v kakom-to vide, parser kart ot "Quake IV".

--- src/common/map_file.rs
+++ src/common/map_file.rs
@@ -25,6 +25,15 @@ pub type MapFileParsed = Vec<Entity>;
 pub struct BrushPlaneQ4
 {
 	pub plane: Plane,
+	pub tex_axis: [TexAxisQ4; 2],
+	pub texture: String,
+}
+
+#[derive(Debug)]
+pub struct TexAxisQ4
+{
+	pub scale: Vec2f,
+	pub offset: f32,
 }
 
 pub type BrushQ4 = Vec<BrushPlaneQ4>;
@@ -84,6 +93,322 @@ pub fn parse_map_file_content(content: Iterator) -> ParseResult<MapFileParsed>
 pub fn parse_map_file_content_q4(content: Iterator) -> ParseResult<MapFileParsedQ4>
 {
 	let mut result = MapFileParsedQ4::new();
+
+	let mut it: Iterator = content;
+
+	let version_keyword = "Version";
+	let expected_version = "3";
+
+	if !it.starts_with(version_keyword)
+	{
+		return Err(ParseError::build(it));
+	}
+	it = &it[version_keyword.len() ..];
+
+	skip_whitespaces(&mut it);
+
+	if !it.starts_with(expected_version)
+	{
+		return Err(ParseError::build(it));
+	}
+	it = &it[expected_version.len() ..];
+
+	while !it.is_empty()
+	{
+		skip_whitespaces(&mut it);
+		if it.starts_with('{')
+		{
+			result.push(parse_entity_q4(&mut it)?);
+		}
+		else
+		{
+			return Err(ParseError::build(it));
+		}
+		skip_whitespaces(&mut it);
+	}
+
+	Ok(result)
+}
+
+fn parse_entity_q4(it: &mut Iterator) -> ParseResult<EntityQ4>
+{
+	*it = &it[1 ..]; // Skip '{'
+
+	let mut result = EntityQ4::default();
+
+	while !it.is_empty() && !it.starts_with('}')
+	{
+		skip_whitespaces(it);
+		if it.starts_with('{')
+		{
+			result.brushes.push(parse_brush_q4(it)?);
+		}
+		else if it.starts_with('"')
+		{
+			let kv = parse_key_value_pair(it)?;
+			result.keys.insert(kv.0, kv.1);
+		}
+		else
+		{
+			return Err(ParseError::build(it));
+		}
+		skip_whitespaces(it);
+	}
+
+	if !it.starts_with('}')
+	{
+		return Err(ParseError::build(it));
+	}
+	*it = &it[1 ..]; // Skip "}"
+
+	Ok(result)
+}
+
+fn parse_brush_q4(it: &mut Iterator) -> ParseResult<BrushQ4>
+{
+	*it = &it[1 ..]; // Skip '{'
+	skip_whitespaces(it);
+
+	let mut result = BrushQ4::new();
+
+	let brush_def = "brushDef3";
+	let patch_def2 = "patchDef2";
+	let patch_def3 = "patchDef3";
+
+	if it.starts_with(brush_def)
+	{
+		*it = &it[brush_def.len() ..];
+		skip_whitespaces(it);
+
+		if !it.starts_with('{')
+		{
+			return Err(ParseError::build(it));
+		}
+		*it = &it[1 ..]; // Skip '{'
+
+		while !it.is_empty() && !it.starts_with('}')
+		{
+			result.push(parse_brush_plane_q4(it)?);
+			skip_whitespaces(it);
+		}
+
+		if !it.starts_with('}')
+		{
+			return Err(ParseError::build(it));
+		}
+		*it = &it[1 ..]; // Skip '}'
+	}
+	else if it.starts_with(patch_def2) || it.starts_with(patch_def3)
+	{
+		*it = &it[patch_def2.len() ..];
+		skip_whitespaces(it);
+
+		if !it.starts_with('{')
+		{
+			return Err(ParseError::build(it));
+		}
+		*it = &it[1 ..]; // Skip '{'
+		skip_whitespaces(it);
+
+		parse_patch_def_body(it)?;
+
+		skip_whitespaces(it);
+		if !it.starts_with('}')
+		{
+			return Err(ParseError::build(it));
+		}
+		*it = &it[1 ..]; // Skip '}'
+	}
+	else
+	{
+		return Err(ParseError::build(it));
+	}
+
+	skip_whitespaces(it);
+	if !it.starts_with('}')
+	{
+		return Err(ParseError::build(it));
+	}
+	*it = &it[1 ..]; // Skip '}'
+
+	Ok(result)
+}
+
+fn parse_brush_plane_q4(it: &mut Iterator) -> ParseResult<BrushPlaneQ4>
+{
+	let plane = parse_plane_equation(it)?;
+	skip_whitespaces(it);
+	let tex_axis = parse_tex_axis_q4(it)?;
+	skip_whitespaces(it);
+	let texture = parse_quoted_string(it)?;
+
+	Ok(BrushPlaneQ4 {
+		plane,
+		texture,
+		tex_axis,
+	})
+}
+
+fn parse_plane_equation(it: &mut Iterator) -> ParseResult<Plane>
+{
+	skip_whitespaces(it);
+	if !it.starts_with('(')
+	{
+		return Err(ParseError::build(it));
+	}
+	*it = &it[1 ..];
+	skip_whitespaces(it);
+
+	let result = Plane {
+		vec: Vec3f::new(parse_number(it)?, parse_number(it)?, parse_number(it)?),
+		dist: parse_number(it)?,
+	};
+
+	skip_whitespaces(it);
+	if !it.starts_with(')')
+	{
+		return Err(ParseError::build(it));
+	}
+	*it = &it[1 ..];
+	skip_whitespaces(it);
+
+	Ok(result)
+}
+
+fn parse_tex_axis_q4(it: &mut Iterator) -> ParseResult<[TexAxisQ4; 2]>
+{
+	skip_whitespaces(it);
+	if !it.starts_with('(')
+	{
+		return Err(ParseError::build(it));
+	}
+	*it = &it[1 ..];
+
+	let v0 = parse_brush_plane_vertex(it)?;
+	let v1 = parse_brush_plane_vertex(it)?;
+
+	let result = [
+		TexAxisQ4 {
+			scale: v0.truncate(),
+			offset: v0.z,
+		},
+		TexAxisQ4 {
+			scale: v1.truncate(),
+			offset: v1.z,
+		},
+	];
+
+	skip_whitespaces(it);
+	if !it.starts_with(')')
+	{
+		return Err(ParseError::build(it));
+	}
+	*it = &it[1 ..];
+
+	Ok(result)
+}
+
+fn parse_patch_def_body(it: &mut Iterator) -> ParseResult<()>
+{
+	let _texture = parse_quoted_string(it)?;
+	parse_patch_header(it)?;
+
+	skip_whitespaces(it);
+
+	if !it.starts_with('(')
+	{
+		return Err(ParseError::build(it));
+	}
+	*it = &it[1 ..];
+
+	while !it.is_empty() && !it.starts_with(')')
+	{
+		skip_whitespaces(it);
+		if !it.starts_with('(')
+		{
+			return Err(ParseError::build(it));
+		}
+		*it = &it[1 ..];
+		skip_whitespaces(it);
+
+		for _i in 0 .. 3
+		{
+			skip_whitespaces(it);
+			parse_patch_control_point(it)?;
+		}
+
+		skip_whitespaces(it);
+		if !it.starts_with(')')
+		{
+			return Err(ParseError::build(it));
+		}
+		*it = &it[1 ..];
+		skip_whitespaces(it);
+	}
+
+	skip_whitespaces(it);
+	if !it.starts_with(')')
+	{
+		return Err(ParseError::build(it));
+	}
+	*it = &it[1 ..];
+
+	Ok(())
+}
+
+fn parse_patch_header(it: &mut Iterator) -> ParseResult<Vec<f32>>
+{
+	skip_whitespaces(it);
+	if !it.starts_with('(')
+	{
+		return Err(ParseError::build(it));
+	}
+	*it = &it[1 ..];
+	skip_whitespaces(it);
+
+	let mut result = Vec::new();
+	while !it.is_empty() && !it.starts_with(')')
+	{
+		result.push(parse_number(it)?);
+		skip_whitespaces(it);
+	}
+
+	skip_whitespaces(it);
+	if !it.starts_with(')')
+	{
+		return Err(ParseError::build(it));
+	}
+	*it = &it[1 ..];
+	skip_whitespaces(it);
+
+	Ok(result)
+}
+
+fn parse_patch_control_point(it: &mut Iterator) -> ParseResult<[f32; 5]>
+{
+	skip_whitespaces(it);
+	if !it.starts_with('(')
+	{
+		return Err(ParseError::build(it));
+	}
+	*it = &it[1 ..];
+	skip_whitespaces(it);
+
+	let result = [
+		parse_number(it)?,
+		parse_number(it)?,
+		parse_number(it)?,
+		parse_number(it)?,
+		parse_number(it)?,
+	];
+
+	skip_whitespaces(it);
+	if !it.starts_with(')')
+	{
+		return Err(ParseError::build(it));
+	}
+	*it = &it[1 ..];
+	skip_whitespaces(it);
 
 	Ok(result)
 }
