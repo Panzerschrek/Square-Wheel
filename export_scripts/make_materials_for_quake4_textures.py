3480ebd865e85306aebc2ed468808cf9e4708e97
Panzerschrek <AntiPanzerschrek@yandex.ru>
2022-05-14 18:06:23 +0300

    V skripte sozdanija materialov dlä tekstur iz "Quake IV" zadajom karty normalej dlä materialov, jesli takovyje imejutsä.

--- export_scripts/make_materials_for_quake4_textures.py
+++ export_scripts/make_materials_for_quake4_textures.py
@@ -3,8 +3,12 @@ import json
 import os
 import sys
 
-def generate_trivial_material_json(texture_file_name):
-	return { "diffuse": texture_file_name }
+def generate_trivial_material_json(diffuse_texture, normal_map_texture):
+	result = dict()
+	result["diffuse"] = diffuse_texture;
+	if not normal_map_texture is None:
+		result["normal_map"] = normal_map_texture;
+	return result
 
 
 def main():
@@ -21,7 +25,10 @@ def main():
 			file_path = os.path.join(os.path.relpath(dir_path, args.input_dir), file)
 			if file_path.endswith("_d.tga"):
 				material_name = "textures/" + file_path.replace("_d.tga", "")
-				result[material_name] = generate_trivial_material_json(file_path)
+				normal_map_file_path = file_path.replace("_d.tga", "_local.tga")
+				if not os.path.exists(os.path.join(args.input_dir, normal_map_file_path)):
+					normal_map_file_path = None
+				result[material_name] = generate_trivial_material_json(file_path, normal_map_file_path)
 
 	with open(args.output_file, mode = "w") as f:
 		f.write(json.dumps(result, indent="\t"))
