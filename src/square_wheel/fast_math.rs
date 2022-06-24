080be7258bd5e146b74886a751e39ede47b8f5d7
Panzerschrek <AntiPanzerschrek@yandex.ru>
2022-06-24 22:21:59 +0300

    Udalena neispoljzujemaja funkçija.

--- src/square_wheel/fast_math.rs
+++ src/square_wheel/fast_math.rs
@@ -213,15 +213,6 @@ mod fast_math_impl
 			])
 		}
 
-		pub fn add(&self, other: &Self) -> Self
-		{
-			Self([
-				self.0[0] + other.0[0],
-				self.0[1] + other.0[1],
-				self.0[2] + other.0[2],
-				self.0[3] + other.0[3],
-			])
-		}
 
 		pub fn mul_scalar_add(&self, scalar: f32, b: &Self) -> Self
 		{
