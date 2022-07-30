3ca05014bddfb3ab95447e7e26911074ceeeb942
Panzerschrek <AntiPanzerschrek@yandex.ru>
2022-07-30 10:27:44 +0300

    Vynos koda opredelenija zatnönnosti v otdeljnyj fajl.

--- src/common/lightmaps_builder.rs
+++ src/common/lightmaps_builder.rs
@@ -1,6 +1,6 @@
 use super::{
-	bsp_map_compact, image, light_cube::*, light_hemisphere::*, lightmap::*, map_file_common, material, math_types::*,
-	pvs, shared_mut_slice::*,
+	bsp_map_compact, image, light_cube::*, light_hemisphere::*, light_trace::*, lightmap::*, map_file_common, material,
+	math_types::*, pvs, shared_mut_slice::*,
 };
 use rayon::prelude::*;
 use std::{io::Write, sync::atomic};
@@ -1175,118 +1175,6 @@ fn get_polygon_center(map: &bsp_map_compact::BSPMap, polygon: &bsp_map_compact::
 	}
 
 	polygon_vertices_average / (polygon.num_vertices as f32)
-}
-
-fn can_see(from: &Vec3f, to: &Vec3f, map: &bsp_map_compact::BSPMap) -> bool
-{
-	let root_node = (map.nodes.len() - 1) as u32;
-	can_see_r(from, to, root_node, map)
-	// TODO - check intersection with submodel polygons?
-}
-
-// Speed-up intersection calculation - recursively determine loction of check edge withing BSP tree.
-// Than check only leafs where edge is actually located.
-fn can_see_r(v0: &Vec3f, v1: &Vec3f, current_index: u32, map: &bsp_map_compact::BSPMap) -> bool
-{
-	if current_index >= bsp_map_compact::FIRST_LEAF_INDEX
-	{
-		let leaf_index = current_index - bsp_map_compact::FIRST_LEAF_INDEX;
-		let leaf = &map.leafs[leaf_index as usize];
-		for i in 0 .. leaf.num_polygons
-		{
-			if edge_intersects_with_polygon(v0, v1, (leaf.first_polygon + i) as usize, map)
-			{
-				return false;
-			}
-		}
-		return true;
-	}
-	else
-	{
-		let node = &map.nodes[current_index as usize];
-		let dist0 = v0.dot(node.plane.vec) - node.plane.dist;
-		let dist1 = v1.dot(node.plane.vec) - node.plane.dist;
-		if dist0 >= 0.0 && dist1 >= 0.0
-		{
-			return can_see_r(v0, v1, node.children[0], map);
-		}
-		if dist0 <= 0.0 && dist1 <= 0.0
-		{
-			return can_see_r(v0, v1, node.children[1], map);
-		}
-
-		// Split edge using BSP node plane.
-
-		let dist_sum = dist1 - dist0;
-		if dist_sum.abs() < MIN_POSITIVE_VALUE
-		{
-			// Edge is almost on polygon plane.
-			return true;
-		}
-		let k0 = dist0 / dist_sum;
-		let k1 = dist1 / dist_sum;
-		let intersection_pos = v0 * k1 - v1 * k0;
-
-		let (v_front, v_back) = if dist0 > 0.0 { (v0, v1) } else { (v1, v0) };
-
-		// HACK!
-		// There is some problems with intersection detection if intersection polygon plane is same as BSP plane.
-		// So, extend edge a little bit behind splitter plane.
-		let eps = 1.0 / 1024.0;
-		let intersection_pos_front = intersection_pos * (1.0 - eps) + v_back * eps;
-		let intersection_pos_back = intersection_pos * (1.0 - eps) + v_front * eps;
-
-		if !can_see_r(v_front, &intersection_pos_front, node.children[0], map)
-		{
-			return false;
-		}
-		if !can_see_r(&intersection_pos_back, v_back, node.children[1], map)
-		{
-			return false;
-		}
-
-		return true;
-	}
-}
-
-fn edge_intersects_with_polygon(v0: &Vec3f, v1: &Vec3f, polygon_index: usize, map: &bsp_map_compact::BSPMap) -> bool
-{
-	let polygon = &map.polygons[polygon_index];
-	let plane = &polygon.plane;
-
-	let dist0 = v0.dot(plane.vec) - plane.dist;
-	let dist1 = v1.dot(plane.vec) - plane.dist;
-	if dist0.signum() == dist1.signum()
-	{
-		// Edge is located at one side of polygon plane.
-		return false;
-	}
-	let dist_sum = dist1 - dist0;
-	if dist_sum.abs() < MIN_POSITIVE_VALUE
-	{
-		// Edge is almost on polygon plane.
-		return false;
-	}
-	let k0 = dist0 / dist_sum;
-	let k1 = dist1 / dist_sum;
-	let intersection_pos = v0 * k1 - v1 * k0;
-
-	// TODO - extend polygon just a bit, to prevent light leakage between adjusted polygons.
-	for i in 0 .. polygon.num_vertices
-	{
-		let v = map.vertices[(polygon.first_vertex + i) as usize];
-		let next_v = map.vertices[(polygon.first_vertex + (i + 1) % polygon.num_vertices) as usize];
-		let edge_vec = next_v - v;
-		let vec_to_instersection_pos = intersection_pos - v;
-		let cross = vec_to_instersection_pos.cross(edge_vec);
-		let normal_dot = cross.dot(plane.vec);
-		if normal_dot < 0.0
-		{
-			return false;
-		}
-	}
-
-	true
 }
 
 pub type SecondaryLightSources = Vec<SecondaryLightSource>;
