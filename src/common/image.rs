d5abe74ad37bc8e884cd917b1f2bcf8e93e7c62a
Panzerschrek <AntiPanzerschrek@yandex.ru>
2022-09-20 12:50:15 +0300

    Realizovana zagruzka izobraženij, s ispoljzovanijem biblioteki "image".

--- src/common/image.rs
+++ src/common/image.rs
@@ -1,5 +1,4 @@
 use super::color::*;
-use sdl2::image::{LoadSurface, SaveSurface};
 
 #[derive(Default, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Image
@@ -10,79 +9,37 @@ pub struct Image
 
 pub fn load(file_path: &std::path::Path) -> Option<Image>
 {
-	let expected_format = sdl2::pixels::PixelFormatEnum::ARGB8888;
-	if let Ok(mut surface) = sdl2::surface::Surface::from_file(file_path)
-	{
-		if surface.pixel_format_enum() != expected_format
-		{
-			if let Ok(surface_converted) = surface.convert_format(expected_format)
-			{
-				surface = surface_converted;
-			}
-			else
-			{
-				return None;
-			}
-		}
+	let src_image = image::open(file_path).ok()?.into_rgba8();
 
-		let row_size = surface.pitch() / 4;
-		let mut image = Image {
-			size: [surface.width(), surface.height()],
-			pixels: vec![Color32::from_rgb(0, 0, 0); (surface.height() * row_size) as usize],
-		};
-
-		surface.with_lock(|pixels| {
-			// TODO - what if alignment is wrong?
-			let pixels_32 = unsafe { pixels.align_to::<Color32>().1 };
-			for y in 0 .. image.size[1]
-			{
-				let dst_start = y * image.size[0];
-				let dst = &mut image.pixels[(dst_start as usize) .. (dst_start + image.size[0]) as usize];
-				let src_start = y * row_size;
-				let src = &pixels_32[(src_start as usize) .. (src_start + image.size[0]) as usize];
-
-				dst.copy_from_slice(src);
-			}
-		});
-
-		return Some(image);
-	}
-
-	None
+	Some(Image {
+		size: [src_image.width(), src_image.height()],
+		pixels: src_image
+			.pixels()
+			.map(|p| Color32::from_rgba(p[0], p[1], p[2], p[3]))
+			.collect(),
+	})
 }
 
 pub fn save(image: &Image, file_path: &std::path::Path) -> bool
 {
-	let element_size = std::mem::size_of::<Color32>();
-	let bytes = unsafe {
-		std::slice::from_raw_parts_mut(
-			(&image.pixels[0]) as *const Color32 as *mut Color32 as *mut u8,
-			element_size * image.pixels.len(),
-		)
-	};
-
-	if let Ok(surface) = sdl2::surface::Surface::from_data(
-		bytes,
+	if let Err(e) = image::save_buffer(
+		file_path,
+		unsafe {
+			std::slice::from_raw_parts(
+				image.pixels.as_ptr() as *const u8,
+				image.pixels.len() * std::mem::size_of::<Color32>(),
+			)
+		},
 		image.size[0],
 		image.size[1],
-		image.size[0] * (element_size as u32),
-		sdl2::pixels::PixelFormatEnum::ARGB8888,
+		image::ColorType::Rgba8,
 	)
 	{
-		match surface.save(file_path)
-		{
-			Ok(()) => true,
-			Err(e) =>
-			{
-				println!("Failed to save image: {}", e);
-				false
-			},
-		}
+		println!("Failed to save image: {}", e);
+		return false;
 	}
-	else
-	{
-		false
-	}
+
+	true
 }
 
 pub fn make_stub() -> Image
