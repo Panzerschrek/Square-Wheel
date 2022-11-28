985c178cdedfceea2d6018c0fe470c7d30332209
Panzerschrek <AntiPanzerschrek@yandex.ru>
2022-11-28 12:11:43 +0300

    Popravleny skripty zapuska.

--- demo/run_demo_generic.sh
+++ demo/run_demo_generic.sh
@@ -1,1 +1,1 @@
-./gnu_linux/square_wheel_generic --exec "resize_window 1280 720" --exec "map demo" --exec "set_view_model p90.iqm notexture"
+./gnu_linux/square_wheel_generic --exec "resize_window 1280 720" --exec "map demo2" --exec "set_view_model p90.iqm notexture"
