a00220022c552eebdfbced3dfb2d669fb0f9f601
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-06-01 16:32:04 +0300

    V skript generaçii materialov dlä tekstur "Quake" dobavlena generaçija izlucajuscego sloja.

--- export_scripts/make_materials_for_quake_textures.py
+++ export_scripts/make_materials_for_quake_textures.py
@@ -3,8 +3,8 @@ import json
 import os
 import sys
 
-def generate_material_json(texture_file_name):
-	# TODO - process sky textures specially. Also generate proper materials for animated textures and deal wirh fullbrights.
+def generate_material_json(textures_dir, texture_file_name):
+	# TODO - process sky textures specially. Also generate proper materials for animated textures.
 	res = { "diffuse": texture_file_name }
 	if texture_file_name.startswith("*"):
 		res["turb"] = { "amplitude" : 4.0, "wave_length" : 64.0, "frequency" : 0.1875 }
@@ -12,6 +12,9 @@ def generate_material_json(texture_file_name):
 			res["roughness"] = 1.0 / 128.0
 			res["blending_mode"] = "Average"
 			res["blocks_view"] = False
+	fb_image_name = os.path.splitext(texture_file_name)[0] + "_fb.tga"
+	if os.path.exists(os.path.join(textures_dir, fb_image_name)):
+		res["emissive_layer"] = { "image" : fb_image_name, "light" : [4.0, 4.0, 4.0] }
 	return res
 
 
@@ -25,7 +28,7 @@ def main():
 	result = dict()
 	for file_name in os.listdir(args.input_dir):
 		name_without_extension = file_name.replace(".tga", "")
-		material_json = generate_material_json(file_name)
+		material_json = generate_material_json(args.input_dir, file_name)
 		result[name_without_extension] = material_json
 
 	with open(args.output_file, mode = "w") as f:
