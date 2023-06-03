68fbd843f953fcc7d914c0e43abdd8efe239fb15
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-06-03 15:02:24 +0300

    Pri zapuske demo-karty vklücajem animaçiju bega modeli igroka.

--- demo/run_demo.sh
+++ demo/run_demo.sh
@@ -1,1 +1,1 @@
-./gnu_linux/square_wheel --exec "resize_window 1280 720" --exec "map demo2" --exec "set_view_model p90.iqm notexture" --exec "set_player_model Dark_Priest.iqm Dark_Priest.png"
+./gnu_linux/square_wheel --exec "resize_window 1280 720" --exec "map demo2" --exec "set_view_model p90.iqm notexture" --exec "set_player_model Dark_Priest.iqm Dark_Priest.png VM_run"
--- demo/run_demo_generic.bat
+++ demo/run_demo_generic.bat
@@ -1,1 +1,1 @@
-"x86_64/square_wheel_generic.exe" --exec "resize_window 1280 720" --exec "map demo2" --exec "set_view_model p90.iqm notexture" --exec "set_player_model Dark_Priest.iqm Dark_Priest.png"
+"x86_64/square_wheel_generic.exe" --exec "resize_window 1280 720" --exec "map demo2" --exec "set_view_model p90.iqm notexture" --exec "set_player_model Dark_Priest.iqm Dark_Priest.png VM_run"
