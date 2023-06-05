73c583e5155e26ff8127f3fd48177474ef983119
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-06-05 10:48:11 +0300

    Dobavleny jescö funkçii materialov.

--- src/common/material_function.rs
+++ src/common/material_function.rs
@@ -30,7 +30,51 @@ pub enum SingleArgumentFunction
 		#[serde(default)]
 		offset: f32,
 	},
-	// TODO - add other functions
+	TriangleWave
+	{
+		// Like sin-wave, but triangular.
+		#[serde(default = "default_one")]
+		frequency: f32,
+
+		#[serde(default)]
+		phase: f32,
+
+		#[serde(default = "default_one")]
+		amplitude: f32,
+
+		#[serde(default)]
+		offset: f32,
+	},
+	SawToothWave
+	{
+		// Like sin-wave, but saw-tooth shaped.
+		#[serde(default = "default_one")]
+		frequency: f32,
+
+		#[serde(default)]
+		phase: f32,
+
+		#[serde(default = "default_one")]
+		amplitude: f32,
+
+		#[serde(default)]
+		offset: f32,
+	},
+	StepWave
+	{
+		// Like sin-wave, but step shaped (first 1, than -1).
+		#[serde(default = "default_one")]
+		frequency: f32,
+
+		#[serde(default)]
+		phase: f32,
+
+		#[serde(default = "default_one")]
+		amplitude: f32,
+
+		#[serde(default)]
+		offset: f32,
+	},
 }
 
 impl Default for SingleArgumentFunction
@@ -47,14 +91,61 @@ impl SingleArgumentFunction
 	{
 		match self
 		{
-			SingleArgumentFunction::Constant(c) => *c,
-			SingleArgumentFunction::Linear { scale, offset } => x * scale + offset,
-			SingleArgumentFunction::SinWave {
+			Self::Constant(c) => *c,
+			Self::Linear { scale, offset } => x * scale + offset,
+			Self::SinWave {
 				frequency,
 				phase,
 				amplitude,
 				offset,
 			} => (x * frequency * std::f32::consts::TAU + phase).sin() * amplitude + offset,
+			Self::TriangleWave {
+				frequency,
+				phase,
+				amplitude,
+				offset,
+			} =>
+			{
+				let arg = x * frequency + phase;
+				let arg_fract = arg - arg.floor();
+				let wave = if arg_fract <= 0.25
+				{
+					4.0 * arg_fract
+				}
+				else if arg_fract <= 0.75
+				{
+					2.0 - 4.0 * arg_fract
+				}
+				else
+				{
+					-4.0 + 4.0 * arg_fract
+				};
+				wave * amplitude + offset
+			},
+			Self::SawToothWave {
+				frequency,
+				phase,
+				amplitude,
+				offset,
+			} =>
+			{
+				let arg = x * frequency + phase;
+				let arg_fract = arg - arg.floor();
+				let wave = arg_fract * 2.0 - 1.0;
+				wave * amplitude + offset
+			},
+			Self::StepWave {
+				frequency,
+				phase,
+				amplitude,
+				offset,
+			} =>
+			{
+				let arg = x * frequency + phase;
+				let arg_fract = arg - arg.floor();
+				let wave = if arg_fract <= 0.5 { 1.0 } else { -1.0 };
+				wave * amplitude + offset
+			},
 		}
 	}
 }
