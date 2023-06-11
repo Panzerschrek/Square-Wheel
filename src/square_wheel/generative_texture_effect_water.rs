3fb8405e20bc0ffa938c5d5d59ef5c6fb33e2bd8
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-06-11 18:38:42 +0300

    Cto-to generirujem.

--- src/square_wheel/generative_texture_effect_water.rs
+++ src/square_wheel/generative_texture_effect_water.rs
@@ -1,5 +1,5 @@
-use super::map_materials_processor_structs::*;
-use crate::common::material::*;
+use super::{map_materials_processor_structs::*, textures::*};
+use crate::common::{color::*, material::*};
 
 pub struct GenerativeTextureEffectWater
 {
@@ -12,7 +12,13 @@ impl GenerativeTextureEffectWater
 {
 	pub fn new(water_effect: WaterEffect) -> Self
 	{
-		let size = 1 << (water_effect.resolution_log2[0] + water_effect.resolution_log2[0]);
+		// TODO - use half-size?
+		let size = 1 << (water_effect.resolution_log2[0] + water_effect.resolution_log2[1]);
+		if size == 0
+		{
+			panic!("Invalid size");
+		}
+
 		Self {
 			water_effect,
 			wave_field_a: vec![0.0; size],
@@ -30,11 +36,11 @@ impl GenerativeTextureEffect for GenerativeTextureEffectWater
 
 	fn update(
 		&mut self,
-		_out_texture_data: &mut GenerativeTextureData,
+		out_texture_data: &mut GenerativeTextureData,
 		_texture_data: &MapTextureData,
 		_all_textures_data: &[MapTextureData],
 		_textures_mapping_table: &[TextureMappingElement],
-		_current_time_s: f32,
+		current_time_s: f32,
 	)
 	{
 		// TODO - use fixed frequency.
@@ -43,10 +49,37 @@ impl GenerativeTextureEffect for GenerativeTextureEffectWater
 			1 << self.water_effect.resolution_log2[1],
 		];
 
+		// Add test emitter.
+		self.wave_field_a[(size[0] / 2 + size[1] / 2 * size[0]) as usize] = (current_time_s * 8.0).sin() * 4.0;
+
 		update_wave_field(size, &mut self.wave_field_b, &self.wave_field_a);
 		update_wave_field(size, &mut self.wave_field_a, &self.wave_field_b);
 
 		// TODO - generate texture itself
+
+		out_texture_data.texture[0].size = size;
+		out_texture_data.texture[0]
+			.pixels
+			.resize((size[0] * size[1]) as usize, TextureElement::default());
+
+		// TODO - perform more complex texture generation, based on wave field.
+		for (dst_texel, wave_value) in out_texture_data.texture[0]
+			.pixels
+			.iter_mut()
+			.zip(self.wave_field_a.iter())
+		{
+			let v = (wave_value * 255.0).max(0.0).min(255.0) as u8;
+			dst_texel.diffuse = Color32::from_rgb(v, v, v);
+		}
+
+		for mip in 1 .. NUM_MIPS
+		{
+			if out_texture_data.texture[mip].pixels.is_empty()
+			{
+				out_texture_data.texture[mip] =
+					super::textures::make_texture(crate::common::image::make_stub(), None, 0.0, None, false);
+			}
+		}
 	}
 }
 
