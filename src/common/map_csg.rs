f955a7a92dfda40780d75b929cddc09768a32add
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-06-18 16:43:43 +0300

    Popytka realizovatj boleje praviljnyje razrezy. Poka cto ono inogda glücit.

--- src/common/map_csg.rs
+++ src/common/map_csg.rs
@@ -2,6 +2,8 @@ use super::{
 	clipping_bsp::*,
 	map_polygonizer::{Brush, MapPolygonized, Polygon},
 	material,
+	math_types::*,
+	plane::*,
 };
 
 #[derive(Debug, Clone)]
@@ -173,5 +175,187 @@ fn cut_polygon_by_brush_planes(polygon: Polygon, brush: &Vec<Polygon>, preserve_
 		};
 	} // for brush planes.
 
+	if result_polygons.len() <= 1
+	{
+		// Cut polygon completely or leave one piece. Preserve this result.
+		return result_polygons;
+	}
+
+	// We can't just return result pieces, because they are cuttet (potentially) very crudely.
+	// We need to cut source polygon, using leftover polygon, preserving cut directions orthogonal.
+	return make_hole_in_polygon(polygon, &leftover_polygon);
+}
+
+// Hole must be inside polygon.
+fn make_hole_in_polygon(polygon: Polygon, hole: &Polygon) -> Vec<Polygon>
+{
+	// Perform cuts from vertices of hole polygon.
+	// Perform cuts only along texture axis.
+
+	// Collect cuts.
+	let possible_cut_plane_normals = [
+		polygon.texture_info.tex_coord_equation[0].vec,
+		-polygon.texture_info.tex_coord_equation[0].vec,
+		polygon.texture_info.tex_coord_equation[1].vec,
+		-polygon.texture_info.tex_coord_equation[1].vec,
+	];
+
+	let num_hole_vertices = hole.vertices.len();
+	let rotation_half_pi = QuaternionF::from_axis_angle(hole.plane.vec.normalize(), Rad(std::f32::consts::PI * 0.5));
+	let mut vertex_cuts = Vec::with_capacity(num_hole_vertices);
+	for i in 0 .. num_hole_vertices
+	{
+		let v_prev = hole.vertices[(i + num_hole_vertices - 1) % num_hole_vertices];
+		let v = hole.vertices[i];
+		let v_next = hole.vertices[(i + 1) % num_hole_vertices];
+		let edge_prev = v - v_prev;
+		let edge = v_next - v;
+
+		let edge_prev_normal = rotation_half_pi.rotate_vector(edge_prev);
+		let edge_normal = rotation_half_pi.rotate_vector(edge);
+
+		let mut selected_cut_plane_vec: Option<Vec3f> = None;
+		for possible_cut_plane_normal in &possible_cut_plane_normals
+		{
+			if edge_prev_normal.dot(*possible_cut_plane_normal) <= 0.0 &&
+				edge_normal.dot(*possible_cut_plane_normal) >= 0.0
+			{
+				selected_cut_plane_vec = Some(*possible_cut_plane_normal);
+				break;
+			}
+		}
+
+		if let Some(selected_cut_plane_vec) = selected_cut_plane_vec
+		{
+			// Ok - select single cut plane for this vertex.
+			vertex_cuts.push(VertexCut::Single(selected_cut_plane_vec));
+		}
+		else
+		{
+			// Sharp angle - must use two cut planes for this vertex.
+			let mut selected_cut_plane_vec_edge_prev: Option<Vec3f> = None;
+			let mut selected_cut_plane_vec_edge: Option<Vec3f> = None;
+			for possible_cut_plane_normal in &possible_cut_plane_normals
+			{
+				if edge_prev_normal.dot(*possible_cut_plane_normal) <= 0.0 && selected_cut_plane_vec_edge_prev.is_none()
+				{
+					selected_cut_plane_vec_edge_prev = Some(*possible_cut_plane_normal);
+				}
+				if edge_normal.dot(*possible_cut_plane_normal) >= 0.0 && selected_cut_plane_vec_edge.is_none()
+				{
+					selected_cut_plane_vec_edge = Some(*possible_cut_plane_normal);
+				}
+			}
+
+			if let (Some(vec_prev), Some(vec)) = (selected_cut_plane_vec_edge_prev, selected_cut_plane_vec_edge)
+			{
+				vertex_cuts.push(VertexCut::Double(vec_prev, vec));
+			}
+			else
+			{
+				// Something went really wrong.
+				return vec![polygon];
+			}
+		}
+	}
+
+	debug_assert!(vertex_cuts.len() == num_hole_vertices);
+
+	// Perform cuts.
+	let mut result_polygons = Vec::new();
+	for i in 0 .. num_hole_vertices
+	{
+		let v = hole.vertices[i];
+		let v_next = hole.vertices[(i + 1) % num_hole_vertices];
+		let edge = v_next - v;
+		let edge_normal = rotation_half_pi.rotate_vector(edge);
+
+		if let VertexCut::Double(vec_prev, vec) = vertex_cuts[i]
+		{
+			// TODO - check this.
+			// Perform double cut for this vertex.
+			let corner_polygon = cut_polygon_by_planes(
+				&polygon,
+				&[
+					Plane {
+						vec: -vec_prev,
+						dist: -vec_prev.dot(v),
+					},
+					Plane {
+						vec: -vec,
+						dist: -vec.dot(v),
+					},
+				],
+			);
+			if corner_polygon.vertices.len() >= 3
+			{
+				result_polygons.push(corner_polygon);
+			}
+		}
+
+		let cut_vec = match vertex_cuts[i]
+		{
+			VertexCut::Single(vec) => vec,
+			VertexCut::Double(_vec_prev, vec) => vec,
+		};
+		let cut_vec_next = match vertex_cuts[(i + 1) % num_hole_vertices]
+		{
+			VertexCut::Single(vec) => -vec,
+			VertexCut::Double(vec_prev, _vec) => vec_prev,
+		};
+
+		let edge_polygon = cut_polygon_by_planes(
+			&polygon,
+			&[
+				Plane {
+					vec: cut_vec,
+					dist: cut_vec.dot(v),
+				},
+				Plane {
+					vec: cut_vec_next,
+					dist: cut_vec_next.dot(v_next),
+				},
+				Plane {
+					vec: edge_normal,
+					dist: edge_normal.dot(v),
+				},
+			],
+		);
+
+		if edge_polygon.vertices.len() >= 3
+		{
+			result_polygons.push(edge_polygon);
+		}
+	}
+
+	if result_polygons.is_empty()
+	{
+		// Something went wrong.
+		return vec![polygon];
+	}
+
 	result_polygons
+}
+
+fn cut_polygon_by_planes(polygon: &Polygon, planes: &[Plane]) -> Polygon
+{
+	let mut result = polygon.clone();
+	for plane in planes
+	{
+		let (front_polygon, _back_polygon) = split_polygon(&result, plane);
+		if front_polygon.vertices.len() < 3
+		{
+			return front_polygon;
+		}
+		result = front_polygon;
+	}
+
+	result
+}
+
+#[derive(Copy, Clone)]
+enum VertexCut
+{
+	Single(Vec3f),
+	Double(Vec3f, Vec3f),
 }
