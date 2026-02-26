0723439f1f30061b087a37b289122d174d9dd66e
Panzerschrek <AntiPanzerschrek@yandex.ru>
2026-02-26 10:41:35 +0300

    Pravki formatirovanija.

--- src/square_wheel/map_visibility_calculator.rs
+++ src/square_wheel/map_visibility_calculator.rs
@@ -54,14 +54,14 @@ impl MapVisibilityCalculator
 		self.current_frame.next();
 		let current_leaf = self.find_current_leaf(camera_matrices);
 
-		for  portal_data in &mut self.portals_data
+		for portal_data in &mut self.portals_data
 		{
-			portal_data.current_frame_projection= None;
+			portal_data.current_frame_projection = None;
 		}
 
-		let current_leaf_ref= &mut self.leafs_data[ current_leaf as usize ];
-		current_leaf_ref.current_frame_bounds= *frame_bounds;
-		current_leaf_ref.visible_frame= self.current_frame;
+		let current_leaf_ref = &mut self.leafs_data[current_leaf as usize];
+		current_leaf_ref.current_frame_bounds = *frame_bounds;
+		current_leaf_ref.visible_frame = self.current_frame;
 
 		let leaf_value = self.map.leafs[current_leaf as usize];
 		for &portal in &self.map.leafs_portals[leaf_value.first_leaf_portal as usize ..
@@ -84,40 +84,40 @@ impl MapVisibilityCalculator
 					portal_value.leafs[0]
 				};
 
-				let next_leaf_ref= &mut self.leafs_data[ next_leaf as usize ];
-				next_leaf_ref.current_frame_bounds= *frame_bounds;
-				next_leaf_ref.visible_frame= self.current_frame;
+				let next_leaf_ref = &mut self.leafs_data[next_leaf as usize];
+				next_leaf_ref.current_frame_bounds = *frame_bounds;
+				next_leaf_ref.visible_frame = self.current_frame;
 			}
 		}
 
-		let root_node_index= bsp_map_compact::get_root_node_index(&self.map);
+		let root_node_index = bsp_map_compact::get_root_node_index(&self.map);
 		self.update_visibility_new_r(root_node_index, camera_matrices)
 	}
 
-	fn update_visibility_new_r(&mut self, node_index : u32, camera_matrices: &CameraMatrices)
+	fn update_visibility_new_r(&mut self, node_index: u32, camera_matrices: &CameraMatrices)
 	{
 		if node_index >= bsp_map_compact::FIRST_LEAF_INDEX
 		{
 			let leaf_index = node_index - bsp_map_compact::FIRST_LEAF_INDEX;
 
-			let leaf_value= &self.map.leafs[leaf_index as usize];
+			let leaf_value = &self.map.leafs[leaf_index as usize];
 
-			let leaf_data= &mut self.leafs_data[leaf_index as usize];
+			let leaf_data = &mut self.leafs_data[leaf_index as usize];
 
 			// Visit all portals of this leaf.
 			// If at least one portal is visible in this frame, assume this leaf to be visible.
 			// Build clipping polygon as intersection of all portal clipping polygons.
 			for &portal in &self.map.leafs_portals[(leaf_value.first_leaf_portal as usize) ..
-					((leaf_value.first_leaf_portal + leaf_value.num_leaf_portals) as usize)]
+				((leaf_value.first_leaf_portal + leaf_value.num_leaf_portals) as usize)]
 			{
-				let portal_data= &self.portals_data[ portal as usize ];
+				let portal_data = &self.portals_data[portal as usize];
 
 				if let Some(portal_current_frame_projection) = &portal_data.current_frame_projection
 				{
 					if leaf_data.visible_frame != self.current_frame
 					{
-						leaf_data.visible_frame= self.current_frame;
-						leaf_data.current_frame_bounds= *portal_current_frame_projection;
+						leaf_data.visible_frame = self.current_frame;
+						leaf_data.current_frame_bounds = *portal_current_frame_projection;
 					}
 					else
 					{
@@ -132,14 +132,15 @@ impl MapVisibilityCalculator
 				for &portal in &self.map.leafs_portals[(leaf_value.first_leaf_portal as usize) ..
 					((leaf_value.first_leaf_portal + leaf_value.num_leaf_portals) as usize)]
 				{
-					let portal_value= &self.map.portals[portal as usize];
+					let portal_value = &self.map.portals[portal as usize];
 
-					if let Some(mut portal_projection) = project_portal(portal_value, &self.map, &camera_matrices.view_matrix)
+					if let Some(mut portal_projection) =
+						project_portal(portal_value, &self.map, &camera_matrices.view_matrix)
 					{
 						portal_projection.intersect(&leaf_data.current_frame_bounds);
 						if portal_projection.is_valid_and_non_empty()
 						{
-							self.portals_data[ portal as usize ].current_frame_projection= Some(portal_projection);
+							self.portals_data[portal as usize].current_frame_projection = Some(portal_projection);
 						}
 					}
 				}
@@ -149,8 +150,11 @@ impl MapVisibilityCalculator
 		{
 			let node = &self.map.nodes[node_index as usize];
 
-			let plane_transformed_w = camera_matrices.planes_matrix.row(3).dot(node.plane.vec.extend(-node.plane.dist));
-			let node_children= node.children;
+			let plane_transformed_w = camera_matrices
+				.planes_matrix
+				.row(3)
+				.dot(node.plane.vec.extend(-node.plane.dist));
+			let node_children = node.children;
 			if plane_transformed_w >= 0.0
 			{
 				self.update_visibility_new_r(node_children[0], camera_matrices);
